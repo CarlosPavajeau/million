44c5b58313f8d5bb7530c4583b4643c0fad2e30d
Carlos Pavajeau <cantte098@gmail.com>
2022-09-18 17:21:31 -0500

    feat: add more store logic

--- store/index.ts
+++ store/index.ts
@@ -2,39 +2,103 @@ import create from 'zustand'
 import { CategoryService } from '../services/category.service'
 import { Category, Question } from '../types'
 import { useEffect } from 'react'
+import { QuestionService } from '../services/question.service'
 
 type GameStore = {
   category?: Category
-  score: number;
+  score: number
 
-  currentQuestion: number;
-  maxQuestions: number;
+  currentQuestion: number
+  maxQuestions: number
 
-  questions: Question[];
+  questions: Question[]
 
-  loadFirstCategory: () => Promise<void>;
+  gameOver: boolean
+
+  loadFirstCategory: () => Promise<void>
+  loadNextCategory: () => Promise<void>
+
+  loadQuestions: () => Promise<void>
 }
 
-export const useGameStore = create<GameStore>(set => ({
+export const useGameStore = create<GameStore>((set, get) => ({
   category: undefined,
-  score: 20,
-  currentQuestion: 2,
+  score: 0,
+  currentQuestion: 1,
   maxQuestions: 5,
   questions: [],
+  gameOver: false,
 
   loadFirstCategory: async () => {
     const category = await CategoryService.fetchFirst()
-    set({category})
-  }
+    set({ category })
+  },
+
+  loadNextCategory: async () => {
+    const current = get().category
+    const category = await CategoryService.fetchNext(current?.difficulty || 0)
+    set({ category })
+  },
+
+  loadQuestions: async () => {
+    const category = get().category
+    if (category) {
+      const questions = await QuestionService.fetchByCategory(category.id)
+      set({ questions })
+    }
+  },
+
+  validateAnswer: async (answerId: number) => {
+    const result = await QuestionService.validateAnswer(answerId)
+    if (result) {
+      const currentQuestion = get().questions[get().currentQuestion]
+      set({ score: get().score + currentQuestion.reward })
+
+      if (get().currentQuestion < get().maxQuestions) {
+        set({ currentQuestion: get().currentQuestion + 1 })
+      } else {
+        await get().loadNextCategory()
+      }
+    } else {
+      set({ gameOver: true })
+    }
+  },
+
+  reset: () => {
+    set({
+      category: undefined,
+      score: 0,
+      currentQuestion: 1,
+      maxQuestions: 5,
+      questions: [],
+      gameOver: false,
+    })
+  },
 }))
 
 export const useCategory = () => {
   const loadFirstCategory = useGameStore(x => x.loadFirstCategory)
-  useEffect(() => {
-    loadFirstCategory().then()
-  }, [loadFirstCategory])
+  const category = useGameStore(x => x.category)
 
-  return useGameStore(x => x.category)
+  useEffect(() => {
+    if (!category) {
+      loadFirstCategory().then()
+    }
+  }, [loadFirstCategory, category])
+
+  return category
 }
 
-export const useGameState = () => useGameStore(state => [state.score, state.currentQuestion, state.maxQuestions])
+type GameState = {
+  score: number
+
+  currentQuestion: number
+  maxQuestions: number
+}
+
+export const useGameState = () =>
+  useGameStore<GameState>(state => ({
+    score: state.score,
+    currentQuestion: state.currentQuestion,
+    maxQuestions: state.maxQuestions,
+  }))
--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,17 @@
 export type Category = {
-    name: string;
-    difficulty: number;
+  id: number
+  name: string
+  difficulty: number
 }
 
 export type Answer = {
-    id: number;
-    content: string;
+  id: number
+  content: string
 }
 
 export type Question = {
-    content: string;
-    reward: number;
-    
-    answers: Answer[];
+  content: string
+  reward: number
+
+  answers: Answer[]
 }
